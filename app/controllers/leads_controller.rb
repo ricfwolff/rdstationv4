5e3a03514a73626200ab30253b7c1e7f4cee2f86
Ricardo Wolff <ricfwolff@yahoo.com.br>
2015-05-25 20:55:26 -0300

    updates

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -62,10 +62,10 @@ class LeadsController < ApplicationController
       @newSFLead = { "FirstName" => @lead.name, "LastName" => @lead.last_name,
                       "Email" => @lead.email, "Company" => @lead.company,
                       "Title" => @lead.job_title, "Phone" => @lead.phone,
-                      "Website" => @lead.website, "Id" => @lead.salesforceid
+                      "Website" => @lead.website
                    }
 
-      @leadId = @client.upsert!("Lead", @newSFLead)
+      @leadId = @client.upsert!("Lead""Id", @lead.salesforceid, @newSFLead)
       if @leadId
         puts @leadId
 
